29de7c905c0dd9146901dc571c2084df2b5d423c
KevinGomes <36061563+KevinGomes@users.noreply.github.com>
2024-05-17 18:40:41 -0400

    Comments
    Added comments to all relevant files

--- database/models/Student.js
+++ database/models/Student.js
@@ -6,28 +6,29 @@ It defines the student model for the database.
 const Sequelize = require('sequelize');  // Import Sequelize
 const db = require('../db');  // Import Sequelize database instance called "db"
 
+// Define the campus model
 const Student = db.define("student", {
   firstname: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, //string, first name of student, not nullable
     allowNull: false
   },
 
   lastname: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, //string, last name of student, not nullable
     allowNull: false
   },
 
   email: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, //string, email address of student, not nullable
     allowNull: false
   },
 
   imageurl: {
-    type: Sequelize.STRING,
+    type: Sequelize.STRING, //string, url of image for student, nullable
   },
 
   gpa: {
-    type: Sequelize.FLOAT,
+    type: Sequelize.FLOAT, //float, grade point average of student, not nullable
     allowNull: false,
     validate: {
       min: 0,
